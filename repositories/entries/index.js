d3911a820981d3a530c2f6cb84228a87240996f2
Diego Lo Iacono <da.loiacono@gmail.com>
2022-06-08 13:05:07 +0200

    Added GetEntryByDate

--- repositories/entries/index.js
+++ repositories/entries/index.js
@@ -1,5 +1,11 @@
 const insertEntry = require("./insertEntry");
 const selectEntryById = require("./selectEntryById");
+const selectEntryByDate = require("./selectEntryByDate");
 const updateEntryById = require("./updateEntryById");
 
-module.exports = { insertEntry, selectEntryById, updateEntryById };
+module.exports = {
+  insertEntry,
+  selectEntryById,
+  selectEntryByDate,
+  updateEntryById,
+};
