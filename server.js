dc080582ad7c79b2be039ce2f9f0202fedfc0d93
Diego Lo Iacono <da.loiacono@gmail.com>
2022-06-13 21:26:59 +0200

    modified server.js

--- server.js
+++ server.js
@@ -31,19 +31,21 @@ const app = express();
 
 app.use(express.json());
 
+// users
 app.post("/users", registerUser);
-app.delete("/users/:idUser", validateAuth);
+app.get("/users/activate/:registrationCode", activateUser);
+app.post("/login", loginUser);
+app.delete("/users/:idUser", validateAuth, checkAdmin, deleteUser);
+// entries
+app.get("/entries", getEntries);
+app.get("/entries/:date", validateAuth, getEntriesByDate);
+app.post("/entries", validateAuth, createEntry);
+app.patch("/entries/:idEntry", validateAuth, editEntry);
+app.delete("/entries/:idEntry", validateAuth, deleteEntry);
+// votes
 app.get("/votes", validateAuth, getVotes);
 app.post("/vote", validateAuth, insertVote);
 app.delete("/vote", validateAuth, deleteVote);
-app.get("/users/activate/:registrationCode", activateUser);
-app.post("/login", loginUser);
-app.post("/entries", validateAuth, createEntry);
-app.get("/entries", getEntries);
-app.get("/entries/:date", validateAuth, getEntriesByDate);
-app.patch("/entries/:idEntry", validateAuth, editEntry);
-app.delete("/users/:idUser", validateAuth, checkAdmin, deleteUser);
-app.delete("/entries/:idEntry", validateAuth, deleteEntry);
 
 /** Middleware 404 */
 app.use(notFound);
