11cdbbebf5671441d0cac1a62dc72bb900dd196d
guillelp1996 <guillermo.11@hotmail.com>
2022-07-12 12:55:50 +0200

    vote is fixed, now delete a vote if exist

--- repositories/votes/index.js
+++ repositories/votes/index.js
@@ -1,16 +1,15 @@
-
-
-
-const insertVoteDB = require('./insertVoteDB')
-const checkEntrieById = require('./checkEntrieById')
-const checkUserById = require('./checkUserById')
-const getAllVotes = require('./getAllVotes');
-const deleteVoteDB = require('./deleteVoteDB')
+const insertVoteDB = require("./insertVoteDB");
+const checkEntrieById = require("./checkEntrieById");
+const checkUserById = require("./checkUserById");
+const getAllVotes = require("./getAllVotes");
+const deleteVoteDB = require("./deleteVoteDB");
+const checkVote = require("./checkVote");
 
 module.exports = {
-    insertVoteDB,
-    checkEntrieById,
-    checkUserById,
-    getAllVotes,
-    deleteVoteDB
-}
+  insertVoteDB,
+  checkEntrieById,
+  checkUserById,
+  getAllVotes,
+  deleteVoteDB,
+  checkVote,
+};
